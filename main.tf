ddee39c42e8dd28c824e47b3c17e5ea168c91028
pic16f874 <pic16f874@ukr.net>
2022-07-17 00:12:34 +0300

    fix syntax

--- main.tf
+++ main.tf
@@ -52,5 +52,32 @@ resource "aws_security_group" "allow_http_s" {
   tags = {
     Name = "allow_http_s"
   }
+}
+
+  resource "aws_security_group" "allow_ssh" {
+    name        = "allow_ssh"
+    description = "Allow ssh inbound traffic"
+    vpc_id      = aws_vpc.main.id
+
+    ingress {
+      description = "ssh from VPC"
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+
+    tags = {
+      Name = "allow_ssh"
+    }
+
 
 }
